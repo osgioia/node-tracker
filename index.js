20e187dd76941ef4865cf6d289bab11e5adf9215
Osvaldo Gioia <os.gioia@gmail.com>
2023-06-03 01:30:19 -0400

    bittorrent-tracker added

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+import { Server } from 'bittorrent-tracker';
+import express  from "express"
+
+const app = express();
+const trackerPort = 8080;
+const expressPort = 3000;
+
+app.all("/", (req, res) => {
+  console.log("Just got a request!");
+  res.send("Yo!");
+});
+
+const server = new Server({
+  udp: true,
+  http: true,
+  ws: true,
+  stats: true,
+  filter: (infoHash, params, callback) => {},
+});
+
+app.listen(expressPort, () => {
+    console.log(`Servidor Express.js en ejecución en el puerto ${expressPort}`);
+  });
+
+server.listen(trackerPort, () => {
+  console.log('Tracker en ejecución en el puerto 8080');
+});
