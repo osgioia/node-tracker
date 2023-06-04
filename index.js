6217ab9fac72998d0376acafc309d688d7d8bfd9
Osvaldo Gioia <os.gioia@gmail.com>
2023-06-04 01:57:06 -0400

    addTorrent & checkTorrent

--- index.js
+++ index.js
@@ -7,11 +7,56 @@ const app = express();
 app.use(express.json());
 const expressPort = 3000 || process.env.PORT;
 
+async function addTorrent(infoHash) {
+  try {
+    await prisma.torrent.create({
+      data: {
+        infoHash: infoHash,
+      },
+    });
+    console.log('Torrent agregado correctamente');
+  } catch (error) {
+    console.error('Error al agregar el torrent:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+async function checkTorrent(infoHash) {
+  try {
+    const torrent = await prisma.torrent.findUnique({
+      where: { infoHash },
+    });
+  
+    if (torrent) {
+      await prisma.peer.create({
+        data: {
+          torrentId: torrent.id,
+          ip: params.ip,
+          port: params.port,
+          uploaded: params.uploaded,
+          downloaded: params.downloaded,
+          left: params.left,
+          event: params.event,
+        },
+      });
+      callback(null);
+    } else {
+      throw new Error('Torrent no encontrado');
+    }
+  } catch (error) {
+    callback(error);
+  }
+  
+}
+
 // Ruta POST para agregar torrents
-app.post("/torrents", (req, res) => {
+app.post("/torrents", async (req, res) => {
   const { infoHash } = req.body;
 
   // Agregar lógica para registrar el torrent en la base de datos utilizando Prisma
+  // Llama a la función agregarTorrent y pasa el infoHash correspondiente
+  await addTorrent(infoHash);
 
   res.send("Torrent agregado correctamente");
 });
@@ -21,8 +66,9 @@ const server = new Server({
   http: true,
   ws: true,
   stats: true,
-  filter: (infoHash, params, callback) => {
-    callback(null);
+  filter: async (infoHash, params, callback) => {
+    const cb = await checkTorrent(infoHash)
+    callback(cb);
   },
 });
 
