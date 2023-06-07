bd1cec0f72f5aea554399dac7bc4ff9bd53c4656
Osvaldo Gioia <os.gioia@gmail.com>
2023-06-06 21:27:00 -0400

    fix __dirname

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ import { PrismaClient } from "@prisma/client";
 import fs  from 'fs';
 import path  from 'path';
 import dotenv  from 'dotenv';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 const prisma = new PrismaClient();
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
